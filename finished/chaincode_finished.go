17f7f9821f7a8d92c0be847644bfad3b37125834
Mark Morris <TarantulaTechnology@users.noreply.github.com>
2016-08-29 12:24:03 -0500

    Test commit.

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -1,19 +1,3 @@
-/*
-Copyright IBM Corp 2016 All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
 import (
@@ -23,10 +7,16 @@ import (
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
-// SimpleChaincode example simple Chaincode implementation
+//////////////////////////////////////////////////////////////////////////////
+// SimpleChaincode
+// example simple Chaincode implementation
+//////////////////////////////////////////////////////////////////////////////
 type SimpleChaincode struct {
 }
 
+//////////////////////////////////////////////////////////////////////////////
+// main
+//////////////////////////////////////////////////////////////////////////////
 func main() {
 	err := shim.Start(new(SimpleChaincode))
 	if err != nil {
@@ -34,7 +24,10 @@ func main() {
 	}
 }
 
-// Init resets all the things
+//////////////////////////////////////////////////////////////////////////////
+// Init
+// resets all the things
+//////////////////////////////////////////////////////////////////////////////
 func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
@@ -48,7 +41,10 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 	return nil, nil
 }
 
-// Invoke isur entry point to invoke a chaincode function
+//////////////////////////////////////////////////////////////////////////////
+// Invoke
+// entry point to invoke a chaincode function
+//////////////////////////////////////////////////////////////////////////////
 func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	fmt.Println("invoke is running " + function)
 
@@ -63,7 +59,10 @@ func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args
 	return nil, errors.New("Received unknown function invocation")
 }
 
-// Query is our entry point for queries
+//////////////////////////////////////////////////////////////////////////////
+// Query
+// entry point for queries
+//////////////////////////////////////////////////////////////////////////////
 func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	fmt.Println("query is running " + function)
 
@@ -76,7 +75,10 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
 	return nil, errors.New("Received unknown function query")
 }
 
-// write - invoke function to write key/value pair
+//////////////////////////////////////////////////////////////////////////////
+// write
+// invoke function to write key/value pair
+//////////////////////////////////////////////////////////////////////////////
 func (t *SimpleChaincode) write(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 	var key, value string
 	var err error
@@ -95,7 +97,10 @@ func (t *SimpleChaincode) write(stub *shim.ChaincodeStub, args []string) ([]byte
 	return nil, nil
 }
 
-// read - query function to read key/value pair
+//////////////////////////////////////////////////////////////////////////////
+// read
+// query function to read key/value pair
+//////////////////////////////////////////////////////////////////////////////
 func (t *SimpleChaincode) read(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 	var key, jsonResp string
 	var err error
